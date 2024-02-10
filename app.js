9955a2230546e29cf062f331b4573943671d8cd7
Sarthak Sundriyal <sarthakvsundriyal@gmail.com>
2024-02-10 23:45:25 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const fetch = require('node-fetch');
+require('dotenv').config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+
+//parse html data for POST requests
+app.use(express.urlencoded({ extended:true}));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.render("index");
+});
+app.post('/convert-mp3', async (req, res) => {
+    const videoID = req.body.videoID;
+    if(videoID==undefined || videoID==null || videoID== "") return res.render("index",{success:false,message: "Please enter a valid youtube url"});
+    else{
+        
+        const fetchAPI = await fetch(`https://youtube-mp36.p.rapidapi.com/dl?id=${videoID}`,{
+            "method" : "GET",
+            "headers": {
+                "X-RapidAPI-Key" : process.env.API_KEY,
+                "X-RapidAPI-Host" : process.env.API_HOST
+            }
+        });
+        const response = await fetchAPI.json();
+        if(response.status == "ok") return res.render("index",{success:true,song_title: response.title,song_link:response.link});
+        else{
+            return res.render("index",{success:false,message:response.msg})
+        }
+    } 
+});
+
+app.listen(PORT,()=>{
+    console.log(`Listening on port ${PORT}`);
+});
