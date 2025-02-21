079a39a212ca30acdf900cdbc8935bdfef1f7456
Sarthak Sundriyal <sarthakvsundriyal@gmail.com>
2025-02-21 23:35:24 +0530

    Best alternative discovered!

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const fetch = require('node-fetch');
-require('dotenv').config();
+const ytdl = require('@distube/ytdl-core');
+const ffmpeg = require('fluent-ffmpeg');
+const ffmpegPath = require('ffmpeg-static');
 
 const app = express();
-
+ffmpeg.setFfmpegPath(ffmpegPath);
 const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 
-//parse html data for POST requests
 app.use(express.urlencoded({ extended:true}));
 app.use(express.json());
 
@@ -17,29 +18,33 @@ app.get('/', (req, res) => {
     res.render("index");
 });
 app.post('/convert-mp3', async (req, res) => {
-    const url = req.body.videoID;
-    let uniqueID;
-    if (url.includes('youtu.be')) {
-    uniqueID = (url.match(/youtu\.be\/([^?&]+)/) || [, null])[1];
-    } else {
-        uniqueID = (url.match(/[?&]v=([^&]*)/) || [, null])[1];
-    }
-    if(uniqueID==undefined || uniqueID==null || uniqueID== "") return res.render("index",{success:false,message: "Please enter a valid youtube url"});
-    else{
-        const fetchAPI = await fetch(`https://youtube-mp36.p.rapidapi.com/dl?id=${uniqueID}`,{
-            "method" : "GET",
-            "headers": {
-                "x-rapidapi-key" : process.env.API_KEY,
-                "x-rapidapi-host" : process.env.API_HOST
+    try{
+        const videoUrl = req.body.url;
+        if(!ytdl.validateURL(videoUrl)) {
+            return res.status(400).send('Wrong Youtube URL');
+        }
+        const info = await ytdl.getInfo(videoUrl);
+        const title = info.videoDetails.title.replace(/[^\w\s]/gi,'');
+        res.header('Content-Disposition', `attachment; filename="${title}.mp3"`);
+        res.header('Content-Type','audio/mpeg');
+
+        const audioStream = ytdl(videoUrl,{quality:'highestaudio'});
+        const ffmpegCommand = ffmpeg(audioStream).audioBitrate(128).format('mp3')
+        ffmpegCommand.pipe(res,{end:true});
+        req.on('close', () => {
+            console.log('Client aborted download. Stopping FFmpeg...');
+            ffmpegCommand.kill('SIGKILL'); 
+        });
+        ffmpegCommand.on('error', (err) => {
+            console.error('FFmpeg error:', err);
+            if (!res.headersSent) {
+                res.status(500).send('Conversion Failed');
             }
         });
-        const response = await fetchAPI.json();
-        console.log(response);
-        if(response.status == "ok") return res.render("index",{success:true,song_title: response.title,song_link:response.link});
-        else{
-            return res.render("index",{success:false,message:response.msg})
-        }
-    } 
+    } catch(error){
+        console.error('Error:',error);
+        res.status(500).send('Failed to process request');
+    }
 });
 
 app.listen(PORT,()=>{
